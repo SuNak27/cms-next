955b6d39f68770ebf76dc845cb4c5980704d422c
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2023-03-19 22:51:20 +0700

    Refactor

--- src/layouts/DashLayout/DashLayout.tsx
+++ src/layouts/DashLayout/DashLayout.tsx
@@ -1,29 +1,29 @@
 import { Navbar, Sidebar, SidebarMobile } from "@/component";
-import { Box, Flex, useColorModeValue, useDisclosure } from "@chakra-ui/react";
+import { Box, Flex, useBreakpointValue, useColorModeValue, useDisclosure } from "@chakra-ui/react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 
 export const DashLayout = ({ children }: { children: React.ReactNode }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const router = useRouter();
+  const sideBar = useBreakpointValue({
+    base: <SidebarMobile onClose={onClose} isOpen={isOpen} />,
+    md: <Sidebar onClose={() => onClose} />,
+  });
 
   return (
-    <Box h={'100vh'} bg={useColorModeValue('gray.100', 'gray.900')} >
+    <Box minH={'100vh'} bg={useColorModeValue('gray.100', 'gray.900')}>
       <Flex>
         <Navbar onOpen={onOpen} />
-        <Sidebar
-          onClose={() => onClose}
-          display={{ base: 'none', md: 'block' }}
-        />
       </Flex>
-      <SidebarMobile onClose={onClose} isOpen={isOpen} />
+      {sideBar}
       <Box ml={{ base: 0, md: 60 }} p="4" pt={24}>
         <AnimatePresence mode="wait">
           <motion.div
             key={router.route}
-            initial={{ translateX: 100, opacity: 0 }}
-            animate={{ translateX: 0, opacity: 1 }}
-            exit={{ translateX: 100, opacity: 0, }}
+            initial={{ opacity: 0, }}
+            animate={{ opacity: 1, }}
+            exit={{ opacity: 0, }}
             transition={{ duration: 0.2 }}
           >
             {children}
