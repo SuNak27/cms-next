6807d76b5ff00e887e5b6c9489c833277207773c
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2023-03-24 18:17:30 +0700

    Env

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: "https://dummyjson.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
